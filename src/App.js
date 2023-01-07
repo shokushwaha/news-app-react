2e8101ae963879fa9e83d1a6c4c71d3a3508f52c
Shobhit Kushwaha <shobhitkushwaha1406@gmail.com>
2023-01-07 17:21:06 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Business from './components/Business';
+import Footer from './components/Footer';
+import Health from './components/Health';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+import Science from './components/Science';
+import Sports from './components/Sports';
+import Technology from './components/Technology';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+          <Route path="/sport" element={<Sports />} />
+          <Route path="/science" element={<Science />} />
+          <Route path="/health" element={<Health />} />
+          <Route path="/technology" element={<Technology />} />
+          <Route path="/business" element={<Business />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
